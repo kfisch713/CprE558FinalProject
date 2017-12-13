22ccafdf82be03ef92d2ca5b2e31113ae8877edd
Eric Middleton <ericm@iastate.edu>
2017-12-13 15:20:35 -0600

    Updated HardwareInterface

--- Arduino/HardwareInterface/HardwareInterface.ino
+++ Arduino/HardwareInterface/HardwareInterface.ino
@@ -9,7 +9,6 @@
  *   CH_B: 18
  * DAC:
  *   MOSI: 51
- *   MISO: 50
  *   SCK: 52
  *   Chip Select (CS): 4
  * Zybo:
@@ -18,16 +17,22 @@
   */
 
 const int PIN_CS = 4;
-const unsigned long UPDATE_PERIOD = 10000; //In microseconds
 
-//const float COUNT_TO_METERS = 0.000028947f;
+const unsigned long UPDATE_PERIOD = 1000; //In microseconds
+const unsigned long DEBUG_PERIOD = 100000;
+const unsigned long DAC_TIMEOUT_PERIOD = 100000;
+
+const uint16_t DAC_OFF_VALUE = (1 << 15);
 
 const uint8_t HEADER_VALUE = 0xA5;
 
 int32_t encCount1 = 0, encCount2 = 0;
 uint8_t encState1, encState2;
+uint16_t dacValue;
+uint8_t dacUpdateCount = 0;
 
-unsigned long long updateTime = 0;
+unsigned long long updateTime = 0, debugTime = 0, dacTimeout = 0;
+bool dacActive = false;
 
 void setup() {
   Serial.begin(115200);
@@ -52,35 +57,33 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(21), updateEncoders, CHANGE);
 
   SPI.begin();
+  updateDAC(DAC_OFF_VALUE);
+  dacUpdateCount = 0;
 
   updateTime = micros() + UPDATE_PERIOD;
 }
 
 void loop() {
+  auto curTime = micros();
+  
   noInterrupts();
-  auto countCopy1 = encCount1;
-  auto countCopy2 = encCount2;
+  auto countCopy1 = -encCount1;
+  auto countCopy2 = -encCount2;
   interrupts();
 
-
-
   if(Serial2.available() >= 3) {
-    Serial.print("Available: ");
-    Serial.println(Serial2.available());
-    char ch;
+    int ch;
     while(Serial2.available() >= 3 && (ch = Serial2.read()) != HEADER_VALUE) {
       Serial.print("[Error] Received invalid header value: ");
       Serial.println(ch, HEX);
     }
 
+    dacActive = true;
+    dacTimeout = curTime + DAC_TIMEOUT_PERIOD;
+
     uint16_t dacVal = (Serial2.read() & 0xFF) | (Serial2.read() & 0xFF) << 8;
     updateDAC(dacVal);
-
-    Serial.print("DAC Update: ");
-    Serial.println(dacVal);
   }
-
-  auto curTime = micros();
 
   if(curTime >= updateTime) {
     updateTime += UPDATE_PERIOD;
@@ -88,11 +91,34 @@ void loop() {
     Serial2.write(reinterpret_cast<const char*>(&HEADER_VALUE), sizeof(HEADER_VALUE));
     Serial2.write(reinterpret_cast<char*>(&countCopy1), sizeof(countCopy1));
     Serial2.write(reinterpret_cast<char*>(&countCopy2), sizeof(countCopy2));
+  }
+  if(curTime >= debugTime) {
+    debugTime += DEBUG_PERIOD;
 
     Serial.print("Encoder Update: ");
     Serial.print(countCopy1);
     Serial.print("\t\t");
-    Serial.println(countCopy2); 
+    Serial.println(countCopy2);
+
+    Serial.print("DAC Value: ");
+    Serial.print(dacValue);
+    Serial.print(" (");
+    Serial.print(dacUpdateCount);
+    Serial.println(" DAC updates)\n");
+
+    if(!dacActive) {
+      updateDAC(DAC_OFF_VALUE);
+    }
+    
+    dacUpdateCount = 0;
+  }
+  
+  if(dacActive && curTime >= dacTimeout) {
+    dacActive = false;
+    updateDAC(DAC_OFF_VALUE);
+    dacUpdateCount--;
+
+    Serial.println("DAC Watchdog expired.\n");
   }
 }
 
@@ -113,6 +139,9 @@ void updateDAC(uint16_t value) {
   digitalWrite(PIN_CS, HIGH);
 
   SPI.endTransaction();
+
+  dacValue = value;
+  dacUpdateCount++;
 }
 
 void updateEncoders() {
